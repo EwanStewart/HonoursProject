afb18d4144b637f0beb5964e82ae19b8854377ef
EJ Stewart <80590593+EwanStewart@users.noreply.github.com>
2023-02-13 12:41:05 +0000

    Game progresses after match em up complete, badge unlock implemented with firebase

--- HonoursProject/Assets/Scripts/Wire.cs
+++ HonoursProject/Assets/Scripts/Wire.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 public class Wire : MonoBehaviour
 {
     [SerializeField] private Transform startP;
@@ -12,8 +14,9 @@ public class Wire : MonoBehaviour
     public GameObject lightOn;
     Vector3 startPoint;
     Vector3 startPosition;
-
-
+    public GameObject canvas;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +27,30 @@ public class Wire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        TextMeshProUGUI countText = canvas.transform.GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>();
+        if (countText.text == "3/3")
+        {
+            unlockBadge();
+        }
     }
 
+
+    void unlockBadge()
+    {
+        TextMeshProUGUI countText = canvas.transform.GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>();
+
+        canvas.SetActive(true);
+        countText.gameObject.SetActive(false);
+        Invoke("loadPointers", 1);
+        Debug.Log("a");
+    }
+    
+    void loadPointers()
+    {
+        Debug.Log("Loading Pointers");
+        SceneManager.LoadScene("Pointers");
+    }
+    
     private void OnMouseDrag()
     {
         if (!receiver)
@@ -39,12 +64,17 @@ public class Wire : MonoBehaviour
             {
                 if (collider.gameObject != gameObject)
                 {
-                    Debug.Log(collider.transform.position);
                     updateWire(collider.GetComponent<Wire>().GetAttachPoint());
 
                     if (transform.parent.name.Equals(collider.transform.parent.name))
                     {
+                        TextMeshProUGUI countText = canvas.transform.GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>();
 
+                        char firstChar = countText.text[0];
+                        int count = int.Parse(firstChar.ToString());
+                        count++;
+                        countText.text = count.ToString() + "/3";
+                        
                         collider.GetComponent<Wire>()?.Done();
                         Done();
                     }
---
+++ HonoursProject/Assets/Scripts/handlerBadge.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using Firebase.Database;
+
+public class handlerBadge : MonoBehaviour
+{
+    public GameObject canvas;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextMeshProUGUI countText = canvas.transform.GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>();
+        if (countText.text == "3/3")
+        {
+            unlockBadge();
+        }
+    }
+    
+    void unlockBadge()
+    {
+        TextMeshProUGUI countText = canvas.transform.GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>();
+
+        canvas.SetActive(true);
+        countText.gameObject.SetActive(false);
+        
+        if (PlayerPrefs.HasKey("username"))
+        {
+            FirebaseDatabase.DefaultInstance.GetReference("users").Child(PlayerPrefs.GetString("username")).Child("badges").Child("badge0").SetValueAsync(true);
+        } else
+        {
+            SceneManager.LoadScene("sign-login");
+        }
+        
+        Invoke("loadPointers", 3);
+    }
+    
+    void loadPointers()
+    {
+        Debug.Log("Loading Pointers");
+        SceneManager.LoadScene("Pointers");
+    }
+}
